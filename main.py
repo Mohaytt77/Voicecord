a0b48ebc53d8662fc9f90cf2e377ba56202d64f6
Mohaytt77 <118476667+Mohaytt77@users.noreply.github.com>
2025-04-21 19:40:54 +0100

    main.py

--- main.py
+++ main.py
@@ -31,23 +31,36 @@ discriminator = userinfo["discriminator"]
 userid = userinfo["id"]
 
 def joiner(token, status):
-    ws = websocket.WebSocket()
-    ws.connect('wss://gateway.discord.gg/?v=9&encoding=json')
+    ws = websocket.create_connection('wss://gateway.discord.gg/?v=9&encoding=json')
     start = json.loads(ws.recv())
     heartbeat = start['d']['heartbeat_interval']
-    auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
-    vc = {"op": 4,"d": {"guild_id": GUILD_ID,"channel_id": CHANNEL_ID,"self_mute": SELF_MUTE,"self_deaf": SELF_DEAF}}
+    auth = {
+        "op": 2,
+        "d": {
+            "token": token,
+            "properties": {
+                "$os": "Windows 10",
+                "$browser": "Google Chrome",
+                "$device": "Windows"
+            },
+            "presence": {
+                "status": status,
+                "afk": False
+            }
+        },
+        "s": None,
+        "t": None
+    }
+    vc = {
+        "op": 4,
+        "d": {
+            "guild_id":923212171960262687
+            "channel_id":1094744199712231596
+            "self_mute": SELF_MUTE,
+            "self_deaf": SELF_DEAF
+        }
+    }
     ws.send(json.dumps(auth))
     ws.send(json.dumps(vc))
     time.sleep(heartbeat / 1000)
-    ws.send(json.dumps({"op": 1,"d": None}))
-
-def run_joiner():
-  os.system("clear")
-  print(f"Logged in as {username}#{discriminator} ({userid}).")
-  while True:
-    joiner(usertoken, status)
-    time.sleep(30)
-
-keep_alive()
-run_joiner()
+    ws.send(json.dumps({"op": 1, "d": None}))
