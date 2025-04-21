955de6028e4df3537164edcd8d070740f3cab377
Mohaytt77 <118476667+Mohaytt77@users.noreply.github.com>
2025-04-21 19:52:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ import requests
 import websocket
 from keep_alive import keep_alive
 
-status = "online" #online/dnd/idle
+status = "online"  # online/dnd/idle
 
 guild_id = "923212171960262687"
 channel_id = "1094744199712231596"
 SELF_MUTE = True
 SELF_DEAF = False
 
-usertoken = os.getenv("TOKEN")
+usertoken = os.getenv("TOKEN")  # خزن التوكن هنا من Secrets
 if not usertoken:
-  print("[ERROR] Please add a token inside Secrets.")
-  sys.exit()
+    print("[ERROR] Please add a token inside Secrets.")
+    sys.exit()
 
 headers = {"Authorization": usertoken, "Content-Type": "application/json"}
 
 validate = requests.get('https://canary.discordapp.com/api/v9/users/@me', headers=headers)
 if validate.status_code != 200:
-  print("[ERROR] Your token might be invalid. Please check it again.")
-  sys.exit()
+    print("[ERROR] Your token might be invalid. Please check it again.")
+    sys.exit()
 
 userinfo = requests.get('https://canary.discordapp.com/api/v9/users/@me', headers=headers).json()
 username = userinfo["username"]
@@ -34,6 +34,7 @@ def joiner(token, status):
     ws = websocket.create_connection('wss://gateway.discord.gg/?v=9&encoding=json')
     start = json.loads(ws.recv())
     heartbeat = start['d']['heartbeat_interval']
+
     auth = {
         "op": 2,
         "d": {
@@ -51,16 +52,28 @@ def joiner(token, status):
         "s": None,
         "t": None
     }
+
     vc = {
         "op": 4,
         "d": {
-            "guild_id":923212171960262687
-            "channel_id":1094744199712231596
+            "guild_id": guild_id,
+            "channel_id": channel_id,
             "self_mute": SELF_MUTE,
             "self_deaf": SELF_DEAF
         }
     }
+
     ws.send(json.dumps(auth))
     ws.send(json.dumps(vc))
     time.sleep(heartbeat / 1000)
     ws.send(json.dumps({"op": 1, "d": None}))
+
+def run_joiner():
+    os.system("clear")
+    print(f"Logged in as {username}#{discriminator} ({userid}).")
+    while True:
+        joiner(usertoken, status)
+        time.sleep(30)
+
+keep_alive()
+run_joiner()
